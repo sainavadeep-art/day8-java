a8f50257fcb906cf756a3c55ba31cc8c89b1ea96
sainavadeep-art <chittoorsainavadeep1135@gmail.com>
2024-12-09 12:16:35 +0530

    Add files via upload

---
+++ clear.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Clear
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int z=7-(x+y);
+		System.out.println(z);
+	}
+}
