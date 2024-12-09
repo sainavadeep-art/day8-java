f34b695f5b69b2994228e65681bbc1177a86f235
sainavadeep-art <chittoorsainavadeep1135@gmail.com>
2024-12-09 12:05:36 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Add
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int a = sc.nextInt();
+    		int b = sc.nextInt();
+    		int c = a+b;
+    		System.out.println(c);
+		}
+		
+	}
+}
