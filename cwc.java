aa714aa0f4037cc96d53f80ed6618b3b74bf1a68
sainavadeep-art <chittoorsainavadeep1135@gmail.com>
2024-12-09 11:32:45 +0530

    Add files via upload

---
+++ cwc.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Cwc
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    if(n>=12){
+	        System.out.println("YES");
+	        
+	    }else{
+	        System.out.println("NO");
+	    }
+      
+
+	}
+}
