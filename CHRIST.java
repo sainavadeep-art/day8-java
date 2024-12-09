115cd7fc41ea877fe4da7259a6c58f8c582c770c
sainavadeep-art <chittoorsainavadeep1135@gmail.com>
2024-12-09 11:59:58 +0530

    Add files via upload

---
+++ CHRIST.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Christ
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x==25)
+		{
+		    System.out.println("CHRISTMAS");
+		    
+		}
+		else{
+		    System.out.println("ORDINARY");
+		}
+
+	}
+}
